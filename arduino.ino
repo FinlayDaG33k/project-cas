5b1b745432205bffec4d3e85e46de4556cde7d01
Aroop 'FinlayDaG33k' Roelofs <me@finlaydag33k.nl>
2016-11-21 10:28:22 +0100

    Update arduino.ino

--- arduino.ino
+++ arduino.ino
@@ -15,13 +15,8 @@ JSON default-config
 #include <ArduinoJson.h>
 DynamicJsonBuffer jsonBuffer;
 
-// Constants for all fingers
-const int Vin= 5;                     // Voltage input for resistance measuring
-const float valDetrigger = 43000;     // Resistance in Ohms needed to detrigger a finger.
-const float valTrigger = 45000;       // Resistance in Ohms needed to trigger a finger.
-
 // JSON Config (Don't forget to escape it!)
-char* json = "{\"R1\":{\"sensorPin\": 0,\"sensorResistor\": 20000,\"sensorValue\": 0,\"raw\": 0,\"ledPin\": 4,\"triggered\": false},\"R2\": {\"sensorPin\": 1,\"sensorResistor\": 20000,\"sensorValue\": 0,\"raw\": 0,\"ledPin\": 3,\"triggered\": false},\"R3\": {\"sensorPin\": 2,\"sensorResistor\": 20000,\"sensorValue\": 0,\"raw\": 0,\"ledPin\": 2,\"triggered\": false},\"L1\": {\"sensorPin\": 3,\"sensorResistor\": 20000,\"sensorValue\": 0,\"raw\": 0,\"ledPin\": 5,\"triggered\": false},\"L2\": {\"sensorPin\": 4,\"sensorResistor\": 20000,\"sensorValue\": 0,\"raw\": 0,\"ledPin\": 6,\"triggered\": false},\"L3\": {\"sensorPin\": 5,\"sensorResistor\": 20000,\"sensorValue\": 0,\"raw\": 0,\"ledPin\": 7,\"triggered\": false}}"; // Set the config values in (escaped) JSON
+char* json = "{\"Vin\":5,\"trigger\":45000,\"detrigger\":43000,\"R1\":{\"sensorPin\": 0,\"sensorResistor\": 20000,\"sensorValue\": 0,\"raw\": 0,\"ledPin\": 4,\"triggered\": false},\"R2\": {\"sensorPin\": 1,\"sensorResistor\": 20000,\"sensorValue\": 0,\"raw\": 0,\"ledPin\": 3,\"triggered\": false},\"R3\": {\"sensorPin\": 2,\"sensorResistor\": 20000,\"sensorValue\": 0,\"raw\": 0,\"ledPin\": 2,\"triggered\": false},\"L1\": {\"sensorPin\": 3,\"sensorResistor\": 20000,\"sensorValue\": 0,\"raw\": 0,\"ledPin\": 5,\"triggered\": false},\"L2\": {\"sensorPin\": 4,\"sensorResistor\": 20000,\"sensorValue\": 0,\"raw\": 0,\"ledPin\": 6,\"triggered\": false},\"L3\": {\"sensorPin\": 5,\"sensorResistor\": 20000,\"sensorValue\": 0,\"raw\": 0,\"ledPin\": 7,\"triggered\": false}}"; // Set the config values in (escaped) JSON
 JsonObject& variables = jsonBuffer.parseObject(json); // parse the JSON values in an array called: variables
 
 void finger(char *fingerCode){
@@ -29,18 +24,18 @@ void finger(char *fingerCode){
   float Vout= 0;
   variables[fingerCode]["raw"] = analogRead(variables[fingerCode]["sensorPin"]);
   if(variables[fingerCode]["raw"]){
-    buffer = variables[fingerCode]["raw"].as<int>() * Vin;
+    buffer = variables[fingerCode]["raw"].as<int>() * variables["Vin"].as<int>();
     Vout= (buffer)/1024.0;
-    buffer= (Vin/Vout) -1;
+    buffer= (variables["Vin"].as<int>()/Vout) -1;
     variables[fingerCode]["sensorValue"]= variables[fingerCode]["sensorResistor"].as<float>() * buffer;
-    if(variables[fingerCode]["sensorValue"] > valTrigger){
+    if(variables[fingerCode]["sensorValue"] > variables["trigger"].as<float>()){
       digitalWrite(variables[fingerCode]["ledPin"],HIGH);
       if(variables[fingerCode]["triggered"] == false){
         Serial.println(fingerCode);
         variables[fingerCode]["triggered"] = true;
       }
     }
-    if(variables[fingerCode]["sensorValue"] < valDetrigger){
+    if(variables[fingerCode]["sensorValue"] < variables["detrigger"].as<float>()){
       digitalWrite(variables[fingerCode]["ledPin"],LOW);
       variables[fingerCode]["triggered"] = false;
     }
